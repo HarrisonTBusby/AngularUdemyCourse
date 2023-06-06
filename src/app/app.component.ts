5f30962acf9d3d7abff22d042afa1366a04ca55f
Harrison <Harrisontbusby@gmail.com>
2023-06-06 09:33:25 -0700

    AngularUdemyLecture Repo Initilization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-course';
+  title = 'Angular';
+  data = {
+    title: 'AngularTitle'
+  };
+
+  onLogoClicked(){
+    alert('Crunk');
+  }
+
+  onKeyUp(newTitle:string){
+    this.data.title = newTitle;
+  }
 }
